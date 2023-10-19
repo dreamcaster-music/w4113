66ef83901f7f06432415fefcc6f8b078736d4dda
Wes <wesley.studt@gmail.com>
2023-10-18 19:19:27 -0500

    added some boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,15 @@ import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 function App() {
+
+
 	return (
 		<>
+			<div className="app">
+				<div className="container" data-tauri-drag-region>
+					<input className="console-input" type="text" placeholder="Enter command" />
+				</div>
+			</div>
 		</>
 	);
 }
