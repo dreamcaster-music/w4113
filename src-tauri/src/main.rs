b5087c1d92495182a7aa97933338284e7ae83141
Wes <wesley.studt@gmail.com>
2023-10-23 20:07:19 -0500

    added console interaction

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,58 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use log::debug;
+
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+	use std::f32::consts::PI;
+	use std::thread;
+	use std::time::Duration;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn host_list() -> Vec<String> {
+	debug!("Host list command invoked");
+
+	// get cpal hosts list
+	let hosts = cpal::available_hosts();
+	let mut output_hosts: Vec<String> = Vec::new();
+	debug!("Available hosts:");
+	for host_id in hosts {
+		debug!("{:?}", host_id);
+		output_hosts.push(host_id.name().to_string());
+	}
+	output_hosts
+}
+
+#[tauri::command]
+fn device_list(hostname: String) -> Vec<String> {
+	debug!("Device list command invoked");
+
+	// get cpal hosts list
+	let hosts = cpal::available_hosts();
+	let host_id = hosts
+		.iter()
+		.find(|h| h.name().to_string() == hostname)
+		.unwrap();
+
+	let host = cpal::host_from_id(*host_id).unwrap();
+
+	let devices = host.devices().unwrap();
+	let mut output_devices: Vec<String> = Vec::new();
+	debug!("Available devices:");
+	for device in devices {
+		debug!("{:?}", device.name().unwrap());
+		output_devices.push(device.name().unwrap());
+	}
+	output_devices
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+		.plugin(tauri_plugin_log::Builder::default().build())
+        .invoke_handler(tauri::generate_handler![
+			host_list,
+			device_list,
+		])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,141 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
+import { debug } from "tauri-plugin-log-api";
 
 function App() {
+	const [selectedHost, setSelectedHost] = useState<string>("");
 
+	const outputConsole = (output: string) => {
+		const outputDiv = document.querySelector(".console-output");
+		const outputP = document.createElement("p");
+		outputP.innerHTML = output;
+		outputP.className = "console-command";
+		outputDiv?.appendChild(outputP);
+
+		// scroll to bottom
+		outputDiv?.scrollTo(0, outputDiv.scrollHeight);
+	}
+
+	const outputConsoleError = (output: string) => {
+		const outputDiv = document.querySelector(".console-output");
+		const outputP = document.createElement("p");
+		outputP.innerHTML = output;
+		outputP.className = "console-error";
+		outputDiv?.appendChild(outputP);
+
+		// scroll to bottom
+		outputDiv?.scrollTo(0, outputDiv.scrollHeight);
+	}
+
+	const userConsole = (output: string) => {
+		const outputDiv = document.querySelector(".console-output");
+		const outputP = document.createElement("p");
+		outputP.innerHTML = "> " + output;
+		outputP.className = "console-user";
+		outputDiv?.appendChild(outputP);
+
+		// scroll to bottom
+		outputDiv?.scrollTo(0, outputDiv.scrollHeight);
+	}
+
+	const runCommand = (command: string) => {
+		debug("Command invoked from w4113 console:\n " + command);
+
+		let split = command.split(" ");
+
+		switch (split[0]) {
+			case "help":
+				outputConsole("Available commands: help, clear, about, host, device");
+				break;
+			case "clear":
+				let output = document.querySelector(".console-output");
+				output?.remove();
+				output = document.createElement("div");
+				output.className = "console-output";
+				const container = document.querySelector(".console");
+				container?.appendChild(output);
+
+				userConsole("clear");
+				break;
+			case "about":
+				outputConsole("w4113.exe version 0.0.1");
+				outputConsole("created by: w4113");
+				outputConsole("prepare to get vaporized meatbag");
+				break;
+			case "host":
+				if (split[1] === "list") {
+					invoke("host_list").then((result: any) => {
+						outputConsole("Available Hosts:");
+						for (const host of result) {
+							outputConsole("- " + host);
+						}
+						outputConsole("Use 'host select [hostname]' to select a host.");
+					});
+				} else if (split[1] === "select") {
+					if (split[2]) {
+						let hosts = invoke("host_list").then((result: any) => {
+							if (result.includes(split[2])) {
+								setSelectedHost(split[2]);
+								outputConsole("Selected host: " + split[2]);
+								outputConsole("Use 'device list' to view available devices for this host.");
+							} else {
+								outputConsoleError("Host not found.");
+							}
+						});
+					} else {
+						outputConsoleError("Invalid arguments for 'host select' command.\nSyntax: host select [hostname]");
+					}
+				} else {
+					outputConsoleError("Invalid arguments for 'host' command.\nSyntax: host [list|select] [hostname]");
+				}
+				break;
+			case "device":
+				if (selectedHost === "") {
+					outputConsoleError("No host selected. Use 'host select [hostname]' to select a host.");
+					break;
+				} else {
+					if (split[1] === "list") {
+						invoke("device_list", { hostname: selectedHost }).then((result: any) => {
+							outputConsole("Available Devices:");
+							for (const device of result) {
+								outputConsole("- " + device);
+							}
+							outputConsole("Use 'device select [device]' to select a device.");
+						});
+					} else {
+						outputConsoleError("Invalid arguments for 'device' command.\nSyntax: device [list]");
+					}
+				}
+				break;
+			default:
+				outputConsoleError("Command not found. Type 'help' for a list of commands.");
+				break;
+		}
+	}
+
+	const handleInput = (event: any) => {
+		if (event.key === "Enter") {
+			// Add command to output
+			userConsole(event.target.value);
+
+			// Send command to backend
+			runCommand(event.target.value);
+
+			// Clear input
+			event.target.value = "";
+		}
+	}
 
 	return (
 		<>
 			<div className="app">
 				<div className="container" data-tauri-drag-region>
-					<input className="console-input" type="text" placeholder="Enter command" />
+					<div className="console">
+						<div className="console-output">
+						</div>
+						<input className="console-input" type="text" placeholder="Enter command" onKeyDown={handleInput} />
+					</div>
 				</div>
 			</div>
 		</>
