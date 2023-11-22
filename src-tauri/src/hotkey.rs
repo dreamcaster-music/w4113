9948604036e52afb12f8f45b64c967e44fc61e42
Wes <wesley.studt@gmail.com>
2023-11-22 06:53:23 -0600

    Keyboard listening
    implemented simple keyboard listener for MacOS. Somehow this works, it really seems like it would not, but even though it panics several threads, it does seem to work consistently

--- src-tauri/src/hotkey.rs
+++ src-tauri/src/hotkey.rs
@@ -1,35 +1,72 @@
 // Listen to keyboard events using /dev/input
 
+use std::sync::RwLock;
+
 use hidapi::HidApi;
 use log::{error, debug};
 
+use lazy_static::lazy_static;
+
+lazy_static! {
+	static ref API: HidApi = HidApi::new().unwrap();
+	static ref KEYS: RwLock<Vec<u8>> = RwLock::new(vec![0u8; 8]);
+}
+
 pub fn hid_list() -> Result<Vec<String>, String> {
-	match HidApi::new() {
-		Ok(api) => {
 			let mut devices = Vec::new();
-			for device in api.device_list() {
+			for device in API.device_list() {
 
 				//let device_handle = device.open_device(&api).unwrap();
 				let manufacturer = device.manufacturer_string().unwrap_or("Unknown");
 				let product = device.product_string().unwrap_or("Unknown");
-				// debug!("Manufacturer: {}", manufacturer);
-				// debug!("Product: {}", product);
-				// debug!("Other: {:?}", device);
-				// loop {
-				// 	debug!("Reading");
-				// 	let mut buf = [0u8; 8];
-				// 	device_handle.read(&mut buf).unwrap();
-				// 	debug!("{:?}", buf);
-				// }
+				let serial = device.serial_number().unwrap_or("Unknown");
+				let path = device.path().to_str().unwrap_or("Unknown");
 
-				let device = format!("{} : {}", manufacturer, product);
+				let vendor_id = device.vendor_id();
+				let product_id = device.product_id();
+
+				if vendor_id == 1452 || product_id == 641 {
+					let thread = std::thread::spawn(move || {
+						debug!("Found Apple keyboard: {} : {} : {} : {}", manufacturer, product, serial, path);
+						loop {
+							let handle = device.open_device(&API).unwrap();
+							let mut buf = [0u8; 8];
+							let _ = handle.read(&mut buf);
+							let mut compare = false;
+							if buf[0 as usize] == 1 {
+								debug!("Found key: {:?}", buf);
+								let mut keys = match KEYS.write() {
+									Ok(keys) => keys,
+									Err(e) => {
+										error!("Error getting keys: {}", e);
+										continue;
+									}
+								};
+
+								// compare the buf to the current keys; if they are the same, the thread is redundant and needs to be killed
+								let buf_vec = buf.to_vec();
+								
+								compare = true;
+								for i in 0..8 {
+									if buf_vec[i] != keys[i] {
+										compare = false;
+										break;
+									}
+								}
+
+								*keys = buf_vec;
+							}
+
+							if compare {
+								break;
+							} 
+						}
+						debug!("Ending thread.");
+					});
+				}
+
+				let device = format!("{} : {} : {} : {}", manufacturer, product, serial, path);
 				devices.push(format!("{:?}", device));
 			}
 			return Ok(devices);
-		},
-		Err(e) => {
-			error!("Error opening HID API. {:?}", e);
-			return Err(format!("{:?}", e));
-		},
-	}
 }
