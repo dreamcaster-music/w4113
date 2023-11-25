0c3442690e7d5edc505ffb414de0fc973630ea49
Wes <wesley.studt@gmail.com>
2023-11-24 20:52:24 -0600

    added windows crate
    working on windows keybinds for the interface module

--- src-tauri/src/interface.rs
+++ src-tauri/src/interface.rs
@@ -17,7 +17,9 @@ pub struct Interface {
 	manufacturer: String,
 	product: String,
 	serial: String,
+	#[cfg(target_os = "macos")]
 	handles: Vec<hidapi::HidDevice>,
+
 }
 
 impl Display for Interface {
@@ -65,6 +67,8 @@ fn hash(string: String) -> u32 {
 	return hash;
 }
 
+// MacOS/Linux
+#[cfg(target_os = "macos")]
 pub fn get_interfaces() -> Vec<Interface> {
 	let mut interfaces: Vec<Interface> = Vec::new();
 	for device in API.device_list() {
@@ -103,65 +107,16 @@ pub fn get_interfaces() -> Vec<Interface> {
 	return interfaces;
 }
 
-// pub fn hid_list() -> Result<Vec<String>, String> {
-//     let mut devices = Vec::new();
-//     for device in API.device_list() {
-//         //let device_handle = device.open_device(&api).unwrap();
-//         let manufacturer = device.manufacturer_string().unwrap_or("Unknown");
-//         let product = device.product_string().unwrap_or("Unknown");
-//         let serial = device.serial_number().unwrap_or("Unknown");
-//         let path = device.path().to_str().unwrap_or("Unknown");
-
-//         let vendor_id = device.vendor_id();
-//         let product_id = device.product_id();
-
-//         let valid = vendor_id == 1452 || product_id == 641 || product.contains("USB");
-
-//         if valid {
-//             let thread = std::thread::spawn(move || {
-//                 debug!(
-//                     "Found Apple keyboard: {} : {} : {} : {}",
-//                     manufacturer, product, serial, path
-//                 );
-//                 loop {
-//                     let handle = device.open_device(&API).unwrap();
-//                     let mut buf = [0u8; 8];
-//                     let _ = handle.read(&mut buf);
-//                     let mut compare = false;
-//                     if buf[0 as usize] == 1 {
-//                         debug!("Found key: {:?}", buf);
-//                         let mut keys = match KEYS.write() {
-//                             Ok(keys) => keys,
-//                             Err(e) => {
-//                                 error!("Error getting keys: {}", e);
-//                                 continue;
-//                             }
-//                         };
-
-//                         // compare the buf to the current keys; if they are the same, the thread is redundant and needs to be killed
-//                         let buf_vec = buf.to_vec();
-
-//                         compare = true;
-//                         for i in 0..8 {
-//                             if buf_vec[i] != keys[i] {
-//                                 compare = false;
-//                                 break;
-//                             }
-//                         }
-
-//                         *keys = buf_vec;
-//                     }
-
-//                     if compare {
-//                         break;
-//                     }
-//                 }
-//                 debug!("Ending thread.");
-//             });
-//         }
-
-//         let device = format!("{} : {} : {} : {}", manufacturer, product, serial, path);
-//         devices.push(format!("{:?}", device));
-//     }
-//     return Ok(devices);
-// }
+// Windows
+#[cfg(target_os = "windows")]
+pub fn get_interfaces() -> Vec<Interface> {
+	let mut interfaces: Vec<Interface> = Vec::new();
+	// use winapi to get the device list
+	let mut device_info_set = unsafe { winapi::um::setupapi::SetupDiGetClassDevsA(
+		&winapi::um::winnt::GUID_DEVINTERFACE_HID,
+		std::ptr::null(),
+		std::ptr::null_mut(),
+		winapi::um::setupapi::DIGCF_PRESENT | winapi::um::setupapi::DIGCF_DEVICEINTERFACE,
+	) };
+	return interfaces;
+}
